0f9c413f2d416479e333c5788c035b30bf2b0ce0
mohamed khalil babay <medkhalilbabay@gmail.com>
2020-12-16 11:00:20 +0100

    Update tracagecourbe.py

--- Serveur/tracagecourbe.py
+++ Serveur/tracagecourbe.py
@@ -7,8 +7,8 @@ Created on Wed Dec 16 01:01:31 2020
 
 import matplotlib.pyplot as pt
 import sqlite3 as sq
-# import datetime as dte
-# import matplotlib.dates as ptdte
+import datetime as dte
+import matplotlib.dates as ptdte
 
 
 
@@ -21,15 +21,15 @@ import sqlite3 as sq
 def tracer_courbes_disp_velos(date_deb,date_fin,stations,pas):
     con=sq.connect('donnees/bdd.bd')
     cur=con.cursor()
+    d=date_deb.isoformat()
+    f=date_fin.isoformat()
+    
     for s in stations :
-        cur.execute("SELECT time_ISO,bikes FROM historique WHERE velov_number='velov-'+{} ORDER BY time_ISO".format(chr(s)))
+        cur.execute("SELECT time_ISO,bikes FROM historique WHERE velov_number='velov-'+{} and time_ISO>{} and time_ISO<{} ORDER BY time_ISO".format(chr(s),d,f))
         l=cur.fetchall()
-        while date_deb<l[0][0]:
-            l.pop(0)
-        while date_fin>l[-1][0]:
-            l.pop(-1)
-        L=[l[i]for i in range(0,pas/5,len(l))]   
-        T=[]
+
+        L=[l[i][1]for i in range(0,pas/5,len(l))]   
+        T=[l[i][0] for i in range(0,pas/5,len(l))]
         pt.plot(T,L,label=s)
         
     
